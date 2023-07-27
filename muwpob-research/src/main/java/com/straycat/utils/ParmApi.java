1c73671df879e70d41162bd69d7d2eec264d26d5
Overstars <1635396212@qq.com>
2023-07-27 17:44:10 +0800

    [update]更新

--- muwpob-research/src/main/java/com/straycat/utils/ParmApi.java
+++ muwpob-research/src/main/java/com/straycat/utils/ParmApi.java
@@ -3,14 +3,24 @@ package com.straycat.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class ParmApi {
     protected static final Map<String, String> parmMap = new HashMap<>();
     protected static final Logger logger = LoggerFactory.getLogger("ParmApi");
+    static String fileName;
 
     public static void init() {
-
+        String properPath = Thread.currentThread().getContextClassLoader().getResource("/config/" + fileName).getPath();
+        Properties props = new Properties();
+        FileInputStream fio = null;
+        try {
+            fio = new FileInputStream(properPath);
+        } catch () {
+            
+        }
     }
 }
