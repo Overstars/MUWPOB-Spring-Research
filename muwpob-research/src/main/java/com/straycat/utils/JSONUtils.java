88760f696a8591dc116951adc698ac91222f7748
Overstars <1635396212@qq.com>
2023-08-12 17:43:36 +0800

    [add] JSONUtils

---
+++ muwpob-research/src/main/java/com/straycat/utils/JSONUtils.java
@@ -0,0 +1,21 @@
+package com.straycat.utils;
+
+import java.io.Serializable;
+
+/**
+ * @projectName: muwpob-research
+ * @package: com.straycat.utils
+ * @className: JSONUtils
+ * @author: Overstars
+ * @description:
+ *  1. 提供Java对象和JSON互转（序列化/反序列化）toJSONString/parseObject
+ *  2. 提供JSON对象转URL方法
+ * @reference:
+ *  https://zhuanlan.zhihu.com/p/611015292
+ *  https://zhuanlan.zhihu.com/p/149252373
+ * @date: 2023/8/12 16:35
+ * @version: 1.0
+ */
+public class JSONUtils {
+
+}
