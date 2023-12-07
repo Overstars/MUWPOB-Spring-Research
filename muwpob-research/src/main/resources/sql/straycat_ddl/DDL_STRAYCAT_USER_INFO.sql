4e59021a0bf02a8e0da253feaa7032022f9fdacf
Overstars <1635396212@qq.com>
2023-12-07 18:21:45 +0800

    [update]更新sql

--- muwpob-research/src/main/resources/sql/straycat_ddl/DDL_STRAYCAT_USER_INFO.sql
+++ muwpob-research/src/main/resources/sql/straycat_ddl/DDL_STRAYCAT_USER_INFO.sql
@@ -1,6 +1,8 @@
 -- auto-generated definition
 create table if not exists
     straycat_user_info (
-
-);
+        USER_ID DECIMAL NOT NULL COMMENT '用户ID',
+        LOGIN_NAME varchar(30) NOT NULL COMMENT '登录名',
+        CELL_PHONE varchar(30) DEFAULT NULL COMMENT '手机号码'
+    );
 
--- muwpob-research/src/main/resources/sql/straycat_ddl/DDL_TRACE_MESSAGE.sql
+++ muwpob-research/src/main/resources/sql/straycat_ddl/DDL_TRACE_MESSAGE.sql
@@ -1,6 +1,6 @@
 -- auto-generated definition
 create table if not exists
     straycat_trace_message (
-
-);
+        TRACENO varchar(64) comment '交易流水号'
+    );
 
